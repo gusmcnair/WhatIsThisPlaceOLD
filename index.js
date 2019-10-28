0d1ee483533c72bdc76b5e30209f9a94fd441cfc
Angus McNair <amcnair234@gmail.com>
2019-10-28 17:05:48 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function watchButton () {
-    $("button").on("click", event => {
+    $("#submit-button").on("click", event => {
         event.preventDefault();
         getLocation();
     });
 }
+
+function watchForm(){
+    $(".place-name").submit(event => {
+        event.preventDefault();
+        const locationName = $("#city-name").val() + "+" + $("#state-name").val()
+        getCoordinates(locationName);
+        console.log(locationName);
+    })};
 
 function getLocation() {
     fetch("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAbOJBsHu8NRJnwsjAK_UONAWnDd2eh6LA&considerIp=true")
@@ -14,13 +22,29 @@ function getLocation() {
             })
         .then(responseJson => displayResults(responseJson))
         .catch(err => {
-            $(".census-info").append("No dice, chief.")
+            $(".init-form").hide();
+            $(".location-name").fadeIn(1000);
         });
 }
 
+function getCoordinates(locationName) {
+    console.log(fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${locationName}&key=AIzaSyAbOJBsHu8NRJnwsjAK_UONAWnDd2eh6LA&considerIp=true`)    )
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${locationName}&key=AIzaSyAbOJBsHu8NRJnwsjAK_UONAWnDd2eh6LA&considerIp=true`)
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            } else console.log(response)
+            })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err =>{
+            console.log(responseJson);
+        })
+        }
 
-function responseJson(responseJson){
+
+function displayResults(responseJson){
     $(".census-info").append(responseJson)
 }
 
 watchButton()
+watchForm();
